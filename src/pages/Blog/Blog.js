69f39dbb8bbc69ad8f8d430f36ffa528234fedf5
Shi Min <shimin.loww@gmail.com>
2023-01-10 22:41:45 +0800

    feat(pages): add pages

---
+++ src/pages/Blog/Blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Blog.scss';
+
+function Blog() {
+  return (
+    <div className="blog">
+      Blog
+    </div>
+  );
+}
+
+export default Blog;
---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+import Home from './Home';
+import Tea from './Tea';
+import Explore from './Explore';
+import Blog from './Blog';
+
+export { Home, Tea, Explore, Blog };
